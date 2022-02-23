42629c81effeb89f86c3beaeaff52923d4c5c88d
Emmanuel-Narrys <73914870+Emmanuel-Narrys@users.noreply.github.com>
2022-02-23 02:07:58 +0100

    Mise a jour

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,25 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(MenuSeeder::class);
-        $this->call(MenuItemSeeder::class);
-        $this->call(AnneeScolaireSeeder::class);
-        $this->call(SessionSeeder::class);
-        $this->call(TrimestreSeeder::class);
-        $this->call(SequenceSeeder::class);
-        $this->call(CycleSeeder::class);
-        $this->call(ClasseSeeder::class);
-        $this->call(GroupeMatiereSeeder::class);
-        $this->call(MatiereSeeder::class);
-        $this->call(ParentSeeder::class);
-        $this->call(PaiementSeeder::class);
-        $this->call(TrancheSeeder::class);
-        $this->call(EleveSeeder::class);
-        $this->call(InscriptionSeeder::class);
-        $this->call(EnseignantSeeder::class);
-        $this->call(CoursSeeder::class);
-        $this->call(SettingSeeder::class);
-        $this->call(PermissionSeeder::class);
         $this->call(JourSeeder::class);
     }
 }
